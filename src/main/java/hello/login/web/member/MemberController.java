4311ed7950f5f1eb50bdaf1c639eeb092048a257
ssung <qkrtjdcjf124@gmail.com>
2022-03-03 21:49:26 +0900

    회원가입 기능 구현

---
+++ src/main/java/hello/login/web/member/MemberController.java
@@ -0,0 +1,43 @@
+package hello.login.web.member;
+
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/members")
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        model.addAttribute("member", new Member());
+        return "members/addMemberForm";
+    }
+
+    @PostMapping("/add")
+    public String save(@Valid @ModelAttribute("member") Member member, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "members/addMemberForm";
+        }
+
+        log.info("member = {}", member);
+
+        memberRepository.save(member);
+
+        return "redirect:/";
+    }
+}
